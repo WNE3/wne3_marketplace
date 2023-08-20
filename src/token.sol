77a2698c897e605a3972ff746bdf59b856e7616e
Rajiv620019 <rajiv620019@gmail.com>
2023-08-20 14:12:19 +0530

    Changed func name _beforeTokenTransfer to _update

--- src/token.sol
+++ src/token.sol
@@ -9,20 +9,19 @@ import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract MyToken is ERC1155, Ownable, Pausable, ERC1155Burnable, ERC1155Supply {
-
-    /** 
-    @dev constants
-    */
+    /**
+     * @dev constants
+     */
     uint256 public constant PUBLIC_MINT = 0.02 ether;
     uint256 public constant SPECIAL_LIST_MINT = 0.01 ether;
     uint256 public constant LIMIT_PER_WALLET = 100;
     uint256 public constant MAX_SUPPLY = 1500;
-    
+
     bool public s_publicMintIsOpen = false;
-    bool public s_specialMintIsOpen = false; 
+    bool public s_specialMintIsOpen = false;
 
     mapping(address => bool) allowList;
-    mapping(address => uint) purchasesPerWallet;
+    mapping(address => uint256) purchasesPerWallet;
 
     constructor() ERC1155("") {}
 
@@ -38,34 +37,24 @@ contract MyToken is ERC1155, Ownable, Pausable, ERC1155Burnable, ERC1155Supply {
         _unpause();
     }
 
-    function setMintWindow(bool _publicMint, bool _specialList) 
-        external 
-        onlyOwner
-    {
+    function setMintWindow(bool _publicMint, bool _specialList) external onlyOwner {
         _publicMint = s_publicMintIsOpen;
         _specialList = s_publicMintIsOpen;
     }
 
-    function setMintWindow(address[] calldata _addr) external onlyOwner{
-        for(uint256 i = 0; i < _addr.length; i++){
+    function setMintWindow(address[] calldata _addr) external onlyOwner {
+        for (uint256 i = 0; i < _addr.length; i++) {
             allowList[_addr[i]] = true;
         }
     }
 
-    function publicMint(uint256 id, uint256 amount)
-        public
-        payable 
-        onlyOwner
-    {   
+    function publicMint(uint256 id, uint256 amount) public payable onlyOwner {
         require(s_publicMintIsOpen, "Sorry public mint window is closed");
         require(msg.value == PUBLIC_MINT * amount, "Enough ethers are not sent");
-        mint(id, amount); 
+        mint(id, amount);
     }
 
-    function specialListMint(uint256 id, uint256 amount) 
-        public 
-        payable
-    {   
+    function specialListMint(uint256 id, uint256 amount) public payable {
         require(allowList[msg.sender], "You are not chosen to be special");
         require(s_specialMintIsOpen, "Sorry special mint window is closed");
         require(msg.value == SPECIAL_LIST_MINT * amount, "Enough ethers are not sent");
@@ -84,7 +73,7 @@ contract MyToken is ERC1155, Ownable, Pausable, ERC1155Burnable, ERC1155Supply {
         payable(_addr).transfer(balance);
     }
 
-    function uri(uint256 _id) public view virtual override returns (string memory){
+    function uri(uint256 _id) public view virtual override returns (string memory) {
         require(exists(_id));
         return string(abi.encodePacked(super.uri(_id), Strings.toString(_id), ".json"));
     }
@@ -96,11 +85,14 @@ contract MyToken is ERC1155, Ownable, Pausable, ERC1155Burnable, ERC1155Supply {
         _mintBatch(to, ids, amounts, data);
     }
 
-    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
-        internal
-        whenNotPaused
-        override
-    {
-        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+    function _update(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal virtual override(ERC1155, ERC1155Supply) whenNotPaused {
+        super._update(operator, from, to, ids, amounts, data);
     }
 }
