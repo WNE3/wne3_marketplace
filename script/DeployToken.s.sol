f38d74689010970fdede2260c10ea26d0f64ddfa
Swapnapratim <101515205+Swapnapratim@users.noreply.github.com>
2023-08-31 22:23:36 +0530

    Update DeployToken.s.sol

--- script/DeployToken.s.sol
+++ script/DeployToken.s.sol
@@ -1,1 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.9;
 
+import {Script} from "forge-std/Script.sol";
+import {Token} from "../src/Token.sol";
+
+contract DeployToken is Script {
+    function run() external returns (Token) {
+        vm.startBroadcast();
+        Token token = new Token();
+        vm.stopBroadcast();
+        return token;
+    }
+}
